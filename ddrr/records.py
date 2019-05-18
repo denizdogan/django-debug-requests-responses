3f15ee697211be74cca418f71e805c17aa8c06a5
Deniz Dogan <denizdogan@users.noreply.github.com>
2019-05-18 13:33:59 +0200

    Add XML pretty-printing using lxml

--- ddrr/records.py
+++ ddrr/records.py
@@ -1,10 +1,78 @@
 import json
+import re
+from collections import OrderedDict
+from textwrap import shorten
 
 import attr
 from django.utils.functional import cached_property
 
 from ddrr.utils import collect_request_headers
 from ddrr.utils import collect_response_headers
+
+try:
+    from lxml import etree
+    from lxml.etree import XMLSyntaxError
+except ImportError:
+    etree = None
+    XMLSyntaxError = None
+
+
+def pretty_print_json(content):
+    """
+    Pretty-print a JSON string and return it.
+
+    >>> pretty_print_json('')
+    ''
+    >>> pretty_print_json('foobar')
+    'foobar'
+    >>> pretty_print_json('{"foo":"bar"}')
+    '{\\n  "foo": "bar"\\n}'
+    >>> pretty_print_json('   {"foo"  : "bar"  }')
+    '{\\n  "foo": "bar"\\n}'
+
+    :param content: JSON string
+    :return: Pretty-printed JSON string
+    """
+    try:
+        data = json.loads(content)
+        return json.dumps(data, indent=2)
+    except json.decoder.JSONDecodeError:
+        return content
+
+
+def pretty_print_xml(content):
+    """
+    Pretty-print an XML string and return it.
+
+    >>> pretty_print_xml('')
+    ''
+    >>> pretty_print_xml('<p></u>')
+    '<p></u>'
+    >>> pretty_print_xml('<p><div><b>hel</b>lo!</div></p>')
+    '<p>\\n  <div><b>hel</b>lo!</div>\\n</p>\\n'
+
+    :param content: XML string
+    :return: Pretty-printed XML string
+    """
+    if not etree:
+        return content
+    try:
+        tree = etree.fromstring(content)
+        return etree.tostring(tree, encoding=str, pretty_print=True)
+    except XMLSyntaxError:
+        return content
+
+
+PRETTY_PRINTERS = OrderedDict(
+    ((r"/json", pretty_print_json), (r"/xml", pretty_print_xml))
+)
+
+
+def pretty_print(content, content_type):
+    for regex, handler in PRETTY_PRINTERS.items():
+        if re.match(regex, content_type):
+            return handler(content)
+    return content
 
 
 @attr.s
@@ -21,17 +89,11 @@ class RequestLogRecord(object):
     def body(self):
         content = self.request.body.decode("utf-8")
         # optionally pretty print
-        if self._formatter.pretty and "/json" in self.content_type:
-            # noinspection PyBroadException
-            try:
-                data = json.loads(content)
-                content = json.dumps(data, indent=2)
-            except Exception:
-                pass
+        if self._formatter.pretty:
+            content = pretty_print(content, self.content_type)
         # optionally limit output
-        limit = self._formatter.limit_body
-        if limit and len(content) > limit:
-            content = content[:limit] + "..."
+        if self._formatter.limit_body:
+            content = shorten(content, self._formatter.limit_body, placeholder="...")
         return content
 
     @cached_property
@@ -83,13 +145,12 @@ class ResponseLogRecord(object):
     @cached_property
     def content(self):
         content = self.response.content.decode("utf-8")
-        if self._formatter.pretty and "/json" in self.content_type:
-            # noinspection PyBroadException
-            try:
-                data = json.loads(content)
-                return json.dumps(data, indent=2)
-            except Exception:
-                pass
+        # optionally pretty print
+        if self._formatter.pretty:
+            content = pretty_print(content, self.content_type)
+        # optionally limit output
+        if self._formatter.limit_body:
+            content = shorten(content, self._formatter.limit_body, placeholder="...")
         return content
 
     @cached_property
