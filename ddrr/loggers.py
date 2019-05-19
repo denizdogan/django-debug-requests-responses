6c5fcc36e4e1c0ff90c06d644dc8f94320bcc0d4
Deniz Dogan <denizdogan@users.noreply.github.com>
2019-05-19 18:17:40 +0200

    Move loggers to separate module

---
+++ ddrr/loggers.py
@@ -0,0 +1,4 @@
+import logging
+
+request_logger = logging.getLogger("ddrr-request-logger")
+response_logger = logging.getLogger("ddrr-response-logger")
--- ddrr/middleware.py
+++ ddrr/middleware.py
@@ -1,15 +1,13 @@
-import logging
+from ddrr.loggers import request_logger
+from ddrr.loggers import response_logger
 
 
 class DebugRequestsResponses(object):
-    request_log = logging.getLogger("ddrr-request-logger")
-    response_log = logging.getLogger("ddrr-response-logger")
-
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        self.request_log.debug(request)
+        request_logger.debug(request)
         response = self.get_response(request)
-        self.response_log.debug(response)
+        response_logger.debug(response)
         return response
